84bd8f8b79c617bd3f5a53c7fda5f04a8d2f289e
Barbara Hejduk <hejduk.b@gmail.com>
2021-08-07 13:28:36 +0200

    login, register validation done

--- src/pages/login-register/Login.js
+++ src/pages/login-register/Login.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Menu from '../../components/menu/Menu';
 import Title from '../../components/title/Title';
 import './Login.scss';
@@ -8,13 +9,34 @@ const Login = () => {
     const [password, setPassword] = useState("");
     const [emailValidate, setEmailValidate] = useState(true);
     const [passwordValidate, setPasswordValidate] = useState(true);
+    const [formValidate, setFormValidate] = useState(false);
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        if (!email.includes("@")) {
+            setEmailValidate(false);
+        } else {setEmailValidate(true)}
+        if (password.length < 6) {
+            setPasswordValidate(false);
+        } else {setPasswordValidate(true)}
+        
+        if (emailValidate && passwordValidate && email.length>0 && password.length>0) {
+            setFormValidate(true);
+        } else {setFormValidate(false)}
+        if (formValidate) {
+            setEmail("");
+            setPassword("");
+            console.log("Zalogowano")
+        } else {console.log("Błędy w formularzu")};
+    }
 
     return(
         <div className="login">
             <Menu></Menu>
             <div className="login__wrapper">
                 <Title text="Zaloguj się"/>
-                <form className="login__form">
+                <form className="login__form" onSubmit={handleFormSubmit}>
                     <div className="login__form-inputs">
                         <label className="login__label">Email</label>
                         <input 
@@ -34,7 +56,7 @@ const Login = () => {
                         {passwordValidate ? <p></p> : <p className="login__form-alert">Podane hasło jest za krótkie</p>}
                     </div>
                     <div className="login__buttons">
-                        <button className="login__button">Załóż konto</button>
+                        <Link to="/rejestracja"><button className="login__button">Załóż konto</button></Link>
                         <button type="submit" className="login__button">Zaloguj się</button>
                     </div>
                 </form>
--- src/pages/login-register/Register.js
+++ src/pages/login-register/Register.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Menu from '../../components/menu/Menu';
 import Title from '../../components/title/Title';
 import './Login.scss';
@@ -8,14 +9,42 @@ const Register = () => {
     const [firstPassword, setFirstPassword] = useState("");
     const [secondPassword, setSecondPassword] = useState("");
     const [emailValidate, setEmailValidate] = useState(true);
-    const [passwordValidate, setPasswordValidate] = useState(true);
+    const [firstPasswordValidate, setFirstPasswordValidate] = useState(true);
+    const [secondPasswordValidate, setSecondPasswordValidate] = useState(true);
+    const [formValidate, setFormValidate] = useState(false);
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        if (!email.includes("@")) {
+            setEmailValidate(false);
+        } else {setEmailValidate(true)}
+        if (firstPassword.length < 6) {
+            setFirstPasswordValidate(false);
+        } else {setFirstPasswordValidate(true)}
+        if (secondPassword === firstPassword && secondPassword.length>=6) {
+            setSecondPasswordValidate(true);
+        } else {setSecondPasswordValidate(false)}
+        
+        if (emailValidate && firstPasswordValidate && secondPasswordValidate && email.length>0 && firstPassword.length>0 && secondPassword.length>0) {
+            setFormValidate(true);
+        } else {setFormValidate(false)}
+        if (formValidate) {
+            setEmail("");
+            setFirstPassword("");
+            setSecondPassword("");
+            console.log("Zalogowano")
+        } else {console.log("Błędy w formularzu")};
+
+        console.log(formValidate);
+    }
 
     return(
         <div className="login">
             <Menu></Menu>
             <div className="login__wrapper">
                 <Title text="Załóż konto"/>
-                <form className="login__form">
+                <form className="login__form" onSubmit={handleFormSubmit}>
                     <div className="login__form-inputs">
                         <label className="login__label">Email</label>
                         <input 
@@ -28,21 +57,22 @@ const Register = () => {
                         <label className="login__label">Hasło</label>
                         <input 
                             value={firstPassword}
-                            className={passwordValidate ? "login__input" : "login__input login__input-alert"}
+                            className={firstPasswordValidate ? "login__input" : "login__input login__input-alert"}
                             type="text" 
                             onChange={(e)=> setFirstPassword(e.target.value)}>
                         </input>
+                        {secondPasswordValidate ? <p></p> : <p className="login__form-alert">Podane hasło jest za krótkie</p>}
                         <label className="login__label">Powtórz hasło</label>
                         <input 
                             value={secondPassword}
-                            className={passwordValidate ? "login__input" : "login__input login__input-alert"}
+                            className={secondPasswordValidate ? "login__input" : "login__input login__input-alert"}
                             type="text" 
                             onChange={(e)=> setSecondPassword(e.target.value)}>
                         </input>
-                        {passwordValidate ? <p></p> : <p className="login__form-alert">Podane hasło jest za krótkie</p>}
+                        {secondPasswordValidate ? <p></p> : <p className="login__form-alert">Podane hasło nieprawidłowe</p>}
                     </div>
                     <div className="login__buttons">
-                        <button className="login__button">Zaloguj się</button>
+                        <Link to="/logowanie"><button className="login__button">Zaloguj się</button></Link>
                         <button type="submit" className="login__button">Załóż konto</button>
                     </div>
                 </form>
