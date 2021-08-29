0218b17353279b2e9620e98c8de0cd0bf31a786e
Barbara Hejduk <hejduk.b@gmail.com>
2021-08-29 19:13:31 +0200

    home page mobile version done

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,20 +1,46 @@
 import {scroller} from "react-scroll";
 import { Link } from 'react-router-dom';
 import './Menu.scss';
+import { useState } from "react";
 
 const Menu = () => {
+    const [mobileMenu, setMobileMenu] = useState(false);
     const handleOnClickMenu = (where) => {
         scroller.scrollTo(where);
     };
+    const toggleMobileMenu = () => {
+        setMobileMenu((prevState) => !prevState);
+    }
 
     return (
         <div className="menu">
+            { mobileMenu ? 
+                <div className="menu__hamburger menu__hamburger--cross" onClick={toggleMobileMenu}></div> :
+                <div className="menu__hamburger" onClick={toggleMobileMenu}></div>
+            }
             <div className="menu__login-list">
-                <Link to="/logowanie" className="menu__link"><div className="menu__login">Zaloguj</div></Link>
-                <Link to="/rejestracja" className="menu__link"><div className="menu__login">Załóż konto</div></Link>
+                <Link to="/logowanie" className="menu__link">
+                    <div className="menu__login">Zaloguj</div>
+                </Link>
+                <Link to="/rejestracja" className="menu__link">
+                    <div className="menu__login">Załóż konto</div>
+                </Link>
             </div>
+            { mobileMenu ? 
+                <ul className="menu__mobile-list">
+                    <Link to="/" className="menu__link">
+                        <li className="menu__mobile-item" onClick={()=> {handleOnClickMenu("start"); toggleMobileMenu()}}>Start</li>
+                    </Link>
+                    <li className="menu__mobile-item" onClick={()=> {handleOnClickMenu("steps"); toggleMobileMenu()}}>O co chodzi?</li>
+                    <li className="menu__mobile-item" onClick={()=> {handleOnClickMenu("about"); toggleMobileMenu()}}>O nas</li>
+                    <li className="menu__mobile-item" onClick={()=> {handleOnClickMenu("help"); toggleMobileMenu()}}>Fundacja i organizacje</li>
+                    <li className="menu__mobile-item" onClick={()=> {handleOnClickMenu("contact"); toggleMobileMenu()}}>Kontakt</li>
+                </ul> : null
+            }
             <ul className="menu__item-list">
-                <Link to="/" className="menu__link"><li className="menu__item" onClick={()=> handleOnClickMenu("start")}>Start</li></Link>
+                <Link to="/" className="menu__link">
+                    <li className="menu__item" onClick={()=> handleOnClickMenu("start")}>Start</li>
+                </Link>
                 <li className="menu__item" onClick={()=> handleOnClickMenu("steps")}>O co chodzi?</li>
                 <li className="menu__item" onClick={()=> handleOnClickMenu("about")}>O nas</li>
                 <li className="menu__item" onClick={()=> handleOnClickMenu("help")}>Fundacja i organizacje</li>
--- src/sections/help/Help.js
+++ src/sections/help/Help.js
@@ -9,16 +9,16 @@ const Help = () => {
     return (
         <div className="help" name="help">
             <Title text="Komu pomagamy?"></Title>
-            <ul className="help__recipients-list">
-                <li className="help__recipient" onClick={()=> setRecipients("foundations")}>Fundacjom</li>
-                <li className="help__recipient" onClick={()=> setRecipients("organizations")}>Organizacjom <br/> pozarządowym</li>
-                <li className="help__recipient" onClick={()=> setRecipients("locals")}>Lokalnym <br/> zbiórkom</li>
-            </ul>
             <p className="help__description">
                 W naszej bazie znajdziesz listę zweryfikowanych Fundacji, 
                 z którymi współpracujemy. Możesz sprawdzić czym się zajmują, 
                 komu pomagają i czego potrzebują.
             </p>
+            <ul className="help__recipients-list">
+                <li className="help__recipient" onClick={()=> setRecipients("foundations")}>Fundacjom</li>
+                <li className="help__recipient" onClick={()=> setRecipients("organizations")}>Organizacjom <br/> pozarządowym</li>
+                <li className="help__recipient" onClick={()=> setRecipients("locals")}>Lokalnym <br/> zbiórkom</li>
+            </ul>
             <HelpDetails recipients={recipients}/>
         </div>
     )
